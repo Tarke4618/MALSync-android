578718157de83dd27c9e30109aa1157b842dc6f5
Tarke4618 <uncertain4796@yqg8f.onmicrosoft.com>
2025-12-10 08:35:23 +0530

    fix(di): bind missing repositories in RepositoryModule

--- android/data/src/main/java/com/malsync/android/data/di/RepositoryModule.kt
+++ android/data/src/main/java/com/malsync/android/data/di/RepositoryModule.kt
@@ -1,7 +1,15 @@
 package com.malsync.android.data.di
 
+import com.malsync.android.data.repository.AnimeRepositoryImpl
+import com.malsync.android.data.repository.AuthRepositoryImpl
 import com.malsync.android.data.repository.MappingRepositoryImpl
+import com.malsync.android.data.repository.MultiServiceRepositoryImpl
+import com.malsync.android.data.repository.StreamingSiteRepositoryImpl
+import com.malsync.android.domain.repository.AnimeRepository
+import com.malsync.android.domain.repository.AuthRepository
 import com.malsync.android.domain.repository.MappingRepository
+import com.malsync.android.domain.repository.MultiServiceRepository
+import com.malsync.android.domain.repository.StreamingSiteRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -15,4 +23,24 @@ abstract class RepositoryModule {
     abstract fun bindMappingRepository(
         mappingRepositoryImpl: MappingRepositoryImpl
     ): MappingRepository
+
+    @Binds
+    abstract fun bindAuthRepository(
+        authRepositoryImpl: AuthRepositoryImpl
+    ): AuthRepository
+
+    @Binds
+    abstract fun bindMultiServiceRepository(
+        multiServiceRepositoryImpl: MultiServiceRepositoryImpl
+    ): MultiServiceRepository
+
+    @Binds
+    abstract fun bindAnimeRepository(
+        animeRepositoryImpl: AnimeRepositoryImpl
+    ): AnimeRepository
+
+    @Binds
+    abstract fun bindStreamingSiteRepository(
+        streamingSiteRepositoryImpl: StreamingSiteRepositoryImpl
+    ): StreamingSiteRepository
 }
